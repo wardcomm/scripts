cbafb2e9f38167b7922057a091cf8145c1a33804
root <root@centos7.localdomain>
2019-05-10 17:23:54 -0400

    add -y for docker cd

--- BASH/install_docker-ce.sh
+++ BASH/install_docker-ce.sh
@@ -2,7 +2,7 @@
 yum update
 yum install yum-utils device-mapper-persistent-data lvm2 -y
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce
+yum install docker-ce -y
 systemctl start docker
 systemctl enable docker
 systemctl status docker
