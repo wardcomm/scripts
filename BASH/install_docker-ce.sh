8906bdfb9716ce59073478e1174abe09387d5155
root <root@centos7.localdomain>
2019-05-10 17:22:15 -0400

    added -y and docker pull and openshift and jenkins

--- BASH/install_docker-ce.sh
+++ BASH/install_docker-ce.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 yum update
-yum install yum-utils device-mapper-persistent-data lvm2
+yum install yum-utils device-mapper-persistent-data lvm2 -y
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce
 systemctl start docker
 systemctl enable docker
 systemctl status docker
+docker pull jenkins/jenkins
+docker pull openshift/origin
 docker -v
+docker run -p 8080:8080 -p 50000:50000 jenkins/jenkins:lts
