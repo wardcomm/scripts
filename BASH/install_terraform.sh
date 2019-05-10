6b3dc85409ec4b907f4e6cb994834ca79278cb76
root <root@centos7.localdomain>
2019-05-10 10:51:16 -0700

    adding files and adding .git ignore

--- BASH/install_terraform.sh
+++ BASH/install_terraform.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-wget -q https://releases.hashicorp.com/terraform/0.12.0-rc1/terraform_0.12.0-rc1_linux_amd64.zip
-unzip terraform_0.12.0-rc1_linux_amd64.zip
-mv terraform /usr/local/bin/terraform
+#uncomment if you want most current
+#wget -q https://releases.hashicorp.com/terraform/0.12.0-rc1/terraform_0.12.0-rc1_linux_amd64.zip
+#comment out this one if you want to run most current
+wget -q https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+#unzip terraform_0.12.0-rc1_linux_amd64.zip
+unzip terraform_0.11.13_linux_amd64.zip
+mv -f terraform /usr/local/bin/terraform
 terraform version
+rm -rf terraform*
