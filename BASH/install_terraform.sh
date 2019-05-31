c229b5b5dd3d85816f758cafdae9b00e04b32b08
J. Chad Ward <jchadward@gmail.com>
2019-05-31 15:45:28 -0400

    fix updaet

--- BASH/install_terraform.sh
+++ BASH/install_terraform.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #uncomment if you want most current
+#ver=0.12.0
+ver=0.11.14
+prog=terraform_$ver_linux_amd64.zip
 #wget -q https://releases.hashicorp.com/terraform/0.12.0-rc1/terraform_0.12.0-rc1_linux_amd64.zip
 #comment out this one if you want to run most current
-wget -q https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+# wget -q https://releases.hashicorp.com/terraform/0.11.13/$prog
+wget -q https://releases.hashicorp.com/terraform/$ver/$prog
 #unzip terraform_0.12.0-rc1_linux_amd64.zip
-unzip terraform_0.11.13_linux_amd64.zip
+unzip terraform_$ver_linux_amd64.zip
 mv -f terraform /usr/local/bin/terraform
 terraform version
 rm -rf terraform*
