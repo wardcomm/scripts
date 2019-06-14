c1a3ab7621191f08c97e9dac813a846061e6965b
J. Chad Ward <jchadward@gmail.com>
2019-06-14 15:18:01 -0400

    added ${APP}

--- BASH/install_hashicorp_vault.sh
+++ BASH/install_hashicorp_vault.sh
@@ -18,10 +18,4 @@ unzip -o ${INSTALL_DIR}/$ZIP -d $INSTALL_DIR && rm -v ${INSTALL_DIR}/$ZIP
 rm -rf /root/bin/${APP}
 mv /opt/${APP}/${APP} /root/bin/${APP}
 ${APP} version
-
-# vault -h
-# mkdir -p /opt/${APP}/
-# INSTALL_DIR="${1:-/opt/${APP}}"
-# URL="https://releases.hashicorp.com/mkdir -p /opt/${APP}/"
-# VER="$(curl -sL $URL | grep -v beta | grep -Po "_(\d*\.?){3}" | sed 's/_//' | sort -V | tail -1)"
-# ZIP="${APP}_${VER}_linux_amd64.zip"
+${APP} -autocomplete-install
