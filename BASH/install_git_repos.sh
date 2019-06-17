87baeff523c73071d8e27a9a15d6c0d2a870f9d9
J. Chad Ward <jchadward@gmail.com>
2019-06-17 12:52:06 -0400

    adding github  repos

---
+++ BASH/install_git_repos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir -p /REPO
+cd /REPO
+git clone https://github.com/wardcomm/scripts.git
+git clone https://github.com/wardcomm/aws_terrafrom.git
+git clone https://github.com/wardcomm/linux_logrotate.git
+git clone https://github.com/wardcomm/azure_terraform.git
+git clone https://github.com/wardcomm/linux_update.git
+git clone https://github.com/wardcomm/linux_ansible_control.git
+git clone https://github.com/wardcomm/windows_pm.git
+git clone https://github.com/wardcomm/azure_windows_vm.git
+git clone https://github.com/wardcomm/linux_kerberos.git
+git clone https://github.com/wardcomm/centos-repos.git
+git clone https://github.com/wardcomm/windows_domain.git
+git clone https://github.com/wardcomm/azure_custom.git
+git clone https://github.com/wardcomm/windows_update.git
