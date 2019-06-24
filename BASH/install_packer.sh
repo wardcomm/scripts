7ae5868a825a7dadc355c2ef9b2a2fa8f5feb765
J. Chad Ward <jchadward@gmail.com>
2019-06-24 14:49:50 -0400

    install script for packer

---
+++ BASH/install_packer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+wget https://releases.hashicorp.com/vault/1.1.3/vault_1.1.3_linux_amd64.zip
+
+#!/bin/bash
+#varialbles
+APP="packer"
+INSTALL_DIR="${1:-/opt/${APP}}"
+URL="https://releases.hashicorp.com/${APP}"
+VER="$(curl -sL $URL | grep -v beta | grep -Po "_(\d*\.?){3}" | sed 's/_//' | sort -V | tail -1)"
+ZIP="${APP}_${VER}_linux_amd64.zip"
+#code
+mkdir -p /opt/${APP}/
+${APP} version
+echo "* Downloading ${URL}/${VER}/${APP}_${VER}_linux_amd64.zip"
+curl -s ${URL}/${VER}/${APP}_${VER}_linux_amd64.zip -o ${INSTALL_DIR}/${ZIP}
+echo "* Extracting $ZIP into $INSTALL_DIR"
+unzip -o ${INSTALL_DIR}/$ZIP -d $INSTALL_DIR && rm -v ${INSTALL_DIR}/$ZIP
+rm -rf /root/bin/${APP}
+mv /opt/${APP}/${APP} /root/bin/${APP}
+${APP} version
+${APP} -autocomplete-install
