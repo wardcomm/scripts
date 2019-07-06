2ee4681eabf746fc7e490f733731e66e55faf20f
J. Chad Ward <jchadward@gmail.com>
2019-07-06 14:12:28 -0400

    adding scripts

---
+++ BASH/install_nginx_admin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p /usr/local/src
+cd /usr/local/src
+
+wget http://nginxcp.com/latest/nginxadmin.tar 
+
+tar xf nginxadmin.tar
+
+cd publicnginx
+
+./nginxinstaller install 
---
+++ PowerShell/choco_upgrade.ps1
@@ -0,0 +1,3 @@
+﻿#choco feature enable -name=exitOnRebootDetected
+#choco feature enable -n allowGlobalConfirmation
+choco upgrade chocolatey -y
