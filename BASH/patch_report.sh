9f3056ff583bc860f36f752fe076fe9d0e6ab7d2
J. Chad Ward <Joseph.c.ward@charter.com>
2022-02-09 16:42:22 -0500

    cleaning alignment

--- BASH/patch_report.sh
+++ BASH/patch_report.sh
@@ -7,12 +7,12 @@ server=$hostname
     #   date=$(date +"%B %Y)
 mkdir -p /SRE/scripts
 touch /SRE/scripts/sec-up.csv
-      echo "Server Name, Security Errata,Bugfix,Enhancement" > /SRE/scripts/sec-up.csv
+echo "Server Name, Security Errata,Bugfix,Enhancement" > /SRE/scripts/sec-up.csv
     #  for server in $hostname
     #  do
     #  server=`$server`
-     sec=`yum updateinfo summary | grep 'Security' | grep -v 'Important|Moderate' | tail -1 | awk '{print $1}'`
-     bug=`yum updateinfo summary | grep 'Bugfix' | tail -1 | awk '{print $1}'`
+sec=`yum updateinfo summary | grep 'Security' | grep -v 'Important|Moderate' | tail -1 | awk '{print $1}'`
+bug=`yum updateinfo summary | grep 'Bugfix' | tail -1 | awk '{print $1}'`
 enhance=`yum updateinfo summary | grep 'Enhancement' | tail -1 | awk '{print $1}'`
 echo $hostname,"    security"   $sec,   "   Bug"   $bug,"    enhancement"   $enhance >> /SRE/scripts/sec-up.csv
 echo "Patching Report for `date +"%B %Y"`" | mailx   -s "Patching Report on `date`" -a /SRE/scripts/sec-up.csv joseph.c.ward@charter.com #$email
