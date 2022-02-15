00bc08cc1a51d90d629db1382c50791dea682446
J. Chad Ward <Joseph.c.ward@charter.com>
2022-02-15 14:56:43 -0500

    adding code from bin box

--- BASH/patch_report.sh
+++ BASH/patch_report.sh
@@ -7,18 +7,19 @@ server=$hostname
     #   date=$(date +"%B %Y)
 mkdir -p /SRE/scripts
 touch /SRE/scripts/sec-up.csv
-echo "Server Name, Security Errata,Bugfix,Enhancement" > /SRE/scripts/sec-up.csv
-    #  for server in $hostname
-    #  do
-    #  server=`$server`
 sec=`yum updateinfo summary | grep 'Security' | grep -v 'Important|Moderate' | tail -1 | awk '{print $1}'`
 bug=`yum updateinfo summary | grep 'Bugfix' | tail -1 | awk '{print $1}'`
 enhance=`yum updateinfo summary | grep 'Enhancement' | tail -1 | awk '{print $1}'`
-echo $hostname,"    security"   $sec,   "   Bug"   $bug,"    enhancement"   $enhance >> /SRE/scripts/sec-up.csv
+uptime=`uptime -p`
+env=`echo "${HOSTNAME: -3}" | cut -c -1`
+echo "Server Name,    Security Errata,        Bugfix,       Enhancement,        uptime" > /SRE/scripts/sec-up.csv
+echo $hostname,"       security"   $sec,   "   Bug"   $bug,"    enhancement"   $enhance ,"      uptime $uptime ">> /SRE/scripts/sec-up.csv
+echo $hostname, $sec, $bug,$enhance ,$uptime, $env > /SRE/scripts/security_data.csv
 echo "Patching Report for `date +"%B %Y"`" | mailx   -s "Patching Report on `date`" -a /SRE/scripts/sec-up.csv joseph.c.ward@charter.com #$email
     
 clear
 cat /SRE/scripts/sec-up.csv
+cat /SRE/scripts/security_data.csv
 # rm /SRE/scripts/sec-up.csv
     #  done
     #  echo "Patching Report `date +"%B %Y"` | mailx -s "Patching Report on `date`" -a /SRE/scripts/sec-up.csv $email
