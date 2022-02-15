198162fec4062baef2c7b57f5e6f52501b756d78
J. Chad Ward <Joseph.c.ward@charter.com>
2022-02-15 18:18:12 -0500

    fix director

--- BASH/patch_report.sh
+++ BASH/patch_report.sh
@@ -1,26 +1,32 @@
-      #!/bin/bash
-      #echo "Server Name, Security Errata,Bugfix,Enhancement" > /SRE/scripts/sec-up.csv
+#!/bin/bash
+#echo "Server Name, Security Errata,Bugfix,Enhancement" > /SRE/scripts/sec-up.csv
 email="joseph.c.ward@charter.com"
 hostname="$(hostnamectl |grep 'Static hostname' | awk '{print $3}')"
       # hostname=$hostname
 server=$hostname
     #   date=$(date +"%B %Y)
 mkdir -p /SRE/scripts
-touch /SRE/scripts/sec-up.csv
+mkdir -p /SRE/reports
+touch /SRE/reports/sec-up.csv
+touch /SRE/reports/labels.csv
+touch /SRE/reports/sec-combo.csv
+
 sec=`yum updateinfo summary | grep 'Security' | grep -v 'Important|Moderate' | tail -1 | awk '{print $1}'`
 bug=`yum updateinfo summary | grep 'Bugfix' | tail -1 | awk '{print $1}'`
 enhance=`yum updateinfo summary | grep 'Enhancement' | tail -1 | awk '{print $1}'`
 uptime=`uptime -p`
 env=`echo "${HOSTNAME: -3}" | cut -c -1`
-echo "Server Name,    Security Errata,        Bugfix,       Enhancement,        uptime" > /SRE/scripts/sec-up.csv
-echo $hostname,"       security"   $sec,   "   Bug"   $bug,"    enhancement"   $enhance ,"      uptime "$uptime ,>> /SRE/scripts/sec-up.csv
-echo $hostname, $sec, $bug,$enhance ,$uptime, $env > /SRE/scripts/security_data.csv
-echo "Patching Report for `date +"%B %Y"`" | mailx   -s "Patching Report on `date`" -a /SRE/scripts/sec-up.csv joseph.c.ward@charter.com #$email
+echo "Server Name,    Security Errata,        Bugfix,       Enhancement,        uptime,            Environment" > /SRE/reports/labels.csv
+echo $hostname,"       security"   $sec,   "   Bug"   $bug,"    enhancement"   $enhance ,"      uptime "$uptime ,>> /SRE/reports/sec-combo.csv
+echo $hostname, $sec, $bug,$enhance ,$uptime, $env > /SRE/reports/security_data.csv
+echo "Patching Report for `date +"%B %Y"`" | mailx   -s "Patching Report on `date`" -a /SRE/reports/securit_data.csv joseph.c.ward@charter.com #$email
     
 clear
-cat /SRE/scripts/sec-up.csv
-cat /SRE/scripts/security_data.csv
-# rm /SRE/scripts/sec-up.csv
+cat /SRE/reports/sec-up.csv
+cat /SRE/reports/security_data.csv
+rm /SRE/reports/sec-up.csv
+rm /SRE/reports/labels.csv
+rm /SRE/reports/sec-combo.csv
     #  done
     #  echo "Patching Report `date +"%B %Y"` | mailx -s "Patching Report on `date`" -a /SRE/scripts/sec-up.csv $email
     #  echo "Patching Report for `date +"%B %Y"`" | mailx -s "Patching Report on `date`" -a /SRE/scripts/sec-up.csv $email
