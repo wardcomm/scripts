051f245e1fb503a8aa9950de60e78b6446f72420
root <root@ch-vm-lnx01.corp.orhp.com>
2022-05-20 16:41:07 +0000

    sftp code

---
+++ BASH/orhp_sftp_cnb.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+passhrase="pAvem5entbulgi1nessPoi'ntersma8sherstop;perla6unChpend-inggu[mminesSscarcecu>ddlygru?mbLecas\ually"
+
+sftp   -i /REPO/cnb_private.key  oldrepub@mway.cnb.com:/oldrepub.fromcnb 
