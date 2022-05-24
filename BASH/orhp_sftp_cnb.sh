7d88ec237404290a09f6585d0f9b06a911b24507
J. Chad Ward <joseph.ward@orhp.com>
2022-05-24 14:31:19 -0400

    ` lkdfdlk` quote issue

--- BASH/orhp_sftp_cnb.sh
+++ BASH/orhp_sftp_cnb.sh
@@ -3,4 +3,4 @@ email="joseph.ward@orhp.com"
 passhrase="pAvem5entbulgi1nessPoi'ntersma8sherstop;perla6unChpend-inggu[mminesSscarcecu>ddlygru?mbLecas\ually"
 
 sftp   -i /REPO/cnb_private.key  oldrepub@mway.cnb.com:/oldrepub.fromcnb 
-echo "sftp from cnb on `date +"%B %Y"` | mailx -s "sftp from cnb on `date`"  $email
+echo "sftp from cnb on `date +"%B %Y`" | mailx -s "sftp from cnb on `date`"  $email
