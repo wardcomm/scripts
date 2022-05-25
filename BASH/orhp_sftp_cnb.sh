61285443d745db64b415c0a6a0ea4a062b150a2c
J. Chad Ward <joseph.ward@orhp.com>
2022-05-25 14:13:37 -0400

    adding batch file to snb script

--- BASH/orhp_sftp_cnb.sh
+++ BASH/orhp_sftp_cnb.sh
@@ -2,6 +2,7 @@
 email="joseph.ward@orhp.com"
 passhrase="pAvem5entbulgi1nessPoi'ntersma8sherstop;perla6unChpend-inggu[mminesSscarcecu>ddlygru?mbLecas\ually"
 
-sftp   -i /REPO/cnb_private.key  oldrepub@mway.cnb.com:/oldrepub.fromcnb 
+#sftp   -i /REPO/cnb_private.key  oldrepub@mway.cnb.com:/oldrepub.fromcnb 
+sftp -b /REPO/scripts/BATCH/orhp_cnb_sftp_batch.bat  -i /REPO/cnb_private.key  oldrepub@mway.cnb.com:/oldrepub.fromcnb
 echo "sftp from cnb on `date +"%B %Y`" | mailx -s "sftp from cnb on `date`"  $email
 bye
