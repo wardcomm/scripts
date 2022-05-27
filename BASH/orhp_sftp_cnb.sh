30d4802b0b32f3035254590b4381fcc52f7dbb4e
J. Chad Ward <joseph.ward@orhp.com>
2022-05-27 20:31:37 +0000

    working base script for sftp and cnb orhp

--- BASH/orhp_sftp_cnb.sh
+++ BASH/orhp_sftp_cnb.sh
@@ -2,9 +2,27 @@
 email="joseph.ward@orhp.com"
 passhrase="pAvem5entbulgi1nessPoi'ntersma8sherstop;perla6unChpend-inggu[mminesSscarcecu>ddlygru?mbLecas\ually"
 reply_email="joseph.ward@orhp.com"
-subject="sftp from cnb on `date`" 
-body="SFTP from cnb on `date +%B %Y`"
+the_date="`date +"%B %d %Y"`"
+subject="sftp from cnb on $the_date $TZ_PST"
+TZ_PST="`TZ='America/Los_Angeles' date`"
+TZ_EST="`TZ='America/New_York' date`"
+#data_file="`touch /tmp/data_file`" 
+#report=" `echo '$subject $the_date' > '$data_file'`"
+
+
+echo $TZ_PST
+echo $TZ_EST
+#subject="sftp from cnb on `$date`"
+
+body="SFTP from cnb on $the_date $TZ_PST"
+#body="SFTP from cnb on `$date`"
+
+testing="`cat /etc/hosts`"
+#the_date="`date +%B %Y`"
+attachment="orhp_sftp_cnb.sh"
+#wow="`cat  $data_file`"
+#echo $the_date > $data_file
 
 #sftp   -i /REPO/cnb_private.key  oldrepub@mway.cnb.com:/oldrepub.fromcnb 
 sftp -b /REPO/scripts/BATCH/orhp_cnb_sftp_batch.bat  -i /REPO/cnb_private.key  oldrepub@mway.cnb.com:/oldrepub.fromcnb
-echo "$body" | mailx -s "$subject"  $email -r "$reply_email"
+echo "$body" | mailx -s "$subject"  "$email" -r "$reply_email" # -a "$testing" 
