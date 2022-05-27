97758b8ffdd18988fb1ad5c5e5747261bbe43a55
J. Chad Ward <joseph.ward@orhp.com>
2022-05-27 16:14:25 +0000

    add newer script with working email

--- BASH/orhp_sftp_cnb.sh
+++ BASH/orhp_sftp_cnb.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 email="joseph.ward@orhp.com"
 passhrase="pAvem5entbulgi1nessPoi'ntersma8sherstop;perla6unChpend-inggu[mminesSscarcecu>ddlygru?mbLecas\ually"
+reply_email="joseph.ward@orhp.com"
+subject="sftp from cnb on `date`" 
+body="SFTP from cnb on `date +%B %Y`"
 
 #sftp   -i /REPO/cnb_private.key  oldrepub@mway.cnb.com:/oldrepub.fromcnb 
 sftp -b /REPO/scripts/BATCH/orhp_cnb_sftp_batch.bat  -i /REPO/cnb_private.key  oldrepub@mway.cnb.com:/oldrepub.fromcnb
-echo "sftp from cnb on" 'date +"%B %Y' | mailx -s "sftp from cnb on 'date' "  $email
+echo "$body" | mailx -s "$subject"  $email -r "$reply_email"
