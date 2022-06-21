4de1951a2a580e9fb77bbfee0efeacc3506ee8d1
J. Chad Ward <jchadward@gmail.com>
2022-06-21 12:23:21 -0400

    fixed batch

--- BATCH/orhp_cnb_sftp_batch_upload.bat
+++ BATCH/orhp_cnb_sftp_batch_upload.bat
@@ -1,8 +1,12 @@
 #remote
 ls -t
 pwd
+!echo listing-start
+export today_date=(`date +%m%d%y`)
+export today_file=($today_date"_Lookup7500.csv")
+!echo listing-end
+
 put $today_file
-s
 
 #local
 lpwd
