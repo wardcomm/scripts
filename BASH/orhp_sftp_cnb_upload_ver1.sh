59812bea806d88e8d3fadd801da74392f66edb02
J. Chad Ward <jchadward@gmail.com>
2022-06-22 13:57:16 -0400

    adding est

--- BASH/orhp_sftp_cnb_upload_ver1.sh
+++ BASH/orhp_sftp_cnb_upload_ver1.sh
@@ -68,7 +68,8 @@ $email_date
 $today_file
 
 $TZ_PST
-$TS_EST__________________________" | mailx -s "sftp from cnb on  $TZ_PST"  $email $cc1 $cc2 -r $reply_email
+$TS_EST
+__________________________" | mailx -s "sftp from cnb on  $TZ_PST"  $email $cc1 $cc2 -r $reply_email
 fi
 
 # function clean() {
