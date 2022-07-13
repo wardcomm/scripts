170b31bfb39b4bf8b2af96bc36b099b4c284372c
J. Chad Ward <jchadward@gmail.com>
2022-07-13 14:28:50 -0400

    adding powershell files

---
+++ PowerShell/delete_files_from_unc_location.ps1
@@ -0,0 +1,2 @@
+﻿# Get-ChildItem -Path \\corp.orhp.com\Applications\Environments\Production\Lockbox\Transport -File -Recurse
+Remove-item -Path \\10.17.20.40\dfs\Departments\IT\chad_testing\*.*
--- PowerShell/get_list_of_files_from_unc_location.ps1
+++ PowerShell/get_list_of_files_from_unc_location.ps1
@@ -1,1 +1,2 @@
 ﻿Get-ChildItem -Path \\corp.orhp.com\Applications\Environments\Production\Lockbox\Transport -File -Recurse
+
