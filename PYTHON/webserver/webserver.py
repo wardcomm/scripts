5ec439cd1275b946b75debc3529ccd2ab7b7aa39
J. Chad Ward <jchadward@gmail.com>
2022-10-13 18:20:55 -0400

    test

---
+++ PYTHON/webserver/webserver.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import http.server as httpserver
+import socketserver
+
+def main(port=None):
+	if port is None:
+		port = 8000
+	handler = httpserver.SimpleHTTPRequestHandler
+	try:
+		print("serving at port", port)
+		httpd = socketserver.TCPServer(("", port), handler)
+		print("serving at port", port)
+		httpd.serve_forever()
+	except OSError:
+		print("Given PORT:{} is unavailable.Try running with diffrent PORT Number!".format(port))
+
+if __name__ == '__main__':
+	main()
