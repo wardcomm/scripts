e861e632a8a98eba15fa4b6871d10cd160c80c4a
J. Chad Ward <jchadward@gmail.com>
2022-10-25 09:38:38 -0400

    adding to webserver pyhthon test

--- PYTHON/webserver/webserver.py
+++ PYTHON/webserver/webserver.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python
-import http.server as httpserver
-import socketserver
+# import http.server as httpserver
+# import socketserver
 
-def main(port=None):
-	if port is None:
-		port = 8000
-	handler = httpserver.SimpleHTTPRequestHandler
-	try:
-		print("serving at port", port)
-		httpd = socketserver.TCPServer(("", port), handler)
-		print("serving at port", port)
-		httpd.serve_forever()
-	except OSError:
-		print("Given PORT:{} is unavailable.Try running with diffrent PORT Number!".format(port))
+# def main(port=None):
+# 	if port is None:
+# 		port = 8000
+# 	handler = httpserver.SimpleHTTPRequestHandler
+# 	try:
+# 		print("serving at port", port)
+# 		httpd = socketserver.TCPServer(("", port), handler)
+# 		print("serving at port", port)
+# 		httpd.serve_forever()
+# 	except OSError:
+# 		print("Given PORT:{} is unavailable.Try running with diffrent PORT Number!".format(port))
 
-if __name__ == '__main__':
-	main()
+# if __name__ == '__main__':
+# 	main()
+# server.py
+import http.server # Our http server handler for http requests
+import socketserver # Establish the TCP Socket connections
+ 
+PORT = 9000
+ 
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.path = 'index.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+ 
+Handler = MyHttpRequestHandler
+ 
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Http Server Serving at port", PORT)
+    httpd.serve_forever()
